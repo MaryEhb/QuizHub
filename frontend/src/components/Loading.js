f4a876f70156129819182419bfd769e587f09096
Mariem Ehab <mariem.ehab234@gmail.com>
2024-09-07 20:16:40 +0300

    FRONTEND: Add Loading Animation options and create loading component content

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
+// import loadingAnimation from '../assets/sandwatch.json';
+import loadingAnimation from '../assets/cups.json';
+// import loadingAnimation from '../assets/yellowLoop.json';
+
+// TODO: decide which loading animation to be used and change colors if neccessary 
 
 const Loading = () => {
   return (
-    <div>
-      Loading....
-      {/* TODO: create loading component */}
+    <div className='loading-container'>
+      <Player
+        autoplay
+        loop
+        src={loadingAnimation}
+        style={{ width: '300px' }}
+      />
+      <p>Loading</p>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
