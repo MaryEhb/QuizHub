89f5c2d9840e6ee11e70662b6aa8cf6beb098d43
Mariem Ehab <mariem.ehab234@gmail.com>
2024-09-14 15:26:10 +0300

    FRONTEND: Update Test card component content and styles

--- frontend/src/components/TestCard.js
+++ frontend/src/components/TestCard.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import editImg from '../assets/edit.svg';
+import viewImg from '../assets/vision.png';
 
-const TestCard = ({ test, isOwner, classroomId }) => {
+const TestCard = ({ test, isOwner, classroomId}) => {
   const navigate = useNavigate();
 
   const handleCardClick = () => {
     navigate(`/classrooms/${classroomId}/tests/${test._id}`);
+    console.log(test)
   };
 
   return (
     <div className={`test-card ${isOwner ? 'owner' : 'not-owner'}`}  onClick={handleCardClick} >
+      <div className='icon-container'>
+        {isOwner? (
+          <div className='icon' style={{ backgroundImage: `url(${test.isPublished? viewImg : editImg})` }}></div>
+        ) : (
+          <div>{test.submitScore !== undefined? test.submitScore : '_'}/{test.maxScore? test.maxScore : 0}</div>
+        )}
+      </div>
       <h3>{test.title}</h3>
-      <p>{test.description}</p>
+      <div className='test-info'>
+        <p>Start Date: <span>{test.startTime? new Date(test.startTime).toLocaleDateString() : '_'}</span></p>
+        <p>End Date: <span>{test.endTime? new Date(test.endTime).toLocaleDateString() : '_'}</span></p>
+        {!isOwner && (<p>Last Submit: <span>{test.submitTime? new Date(test.submitTime).toLocaleDateString() : '_'}</span></p>)}
+      </div>
     </div>
   );
 };
